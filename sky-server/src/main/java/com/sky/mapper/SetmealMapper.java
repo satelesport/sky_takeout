17ff96e9f7c792507b933f166b0b2ed76009eeff
satelesport <152187916+satelesport@users.noreply.github.com>
2025-05-24 11:43:16 +0800

    新增套餐分页查询

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,11 +1,15 @@
 package com.sky.mapper;
 
 import com.sky.annotation.AutoFillAnnotation;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface SetmealMapper {
@@ -24,4 +28,6 @@ public interface SetmealMapper {
      */
     @AutoFillAnnotation(OperationType.INSERT)
     void save(Setmeal setmeal);
+
+    List<SetmealVO> queryByName(SetmealPageQueryDTO setmealPageQueryDTO);
 }
