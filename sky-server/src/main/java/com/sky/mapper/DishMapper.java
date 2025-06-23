6f5ee971f83a680fdddf803aa4e052a915176b8d
satelesport <2194743140@qq.com>
2025-06-23 23:15:39 +0800

    新增工作台相关代码

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -12,6 +12,7 @@ import org.apache.ibatis.annotations.Select;
 
 import java.awt.*;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -71,4 +72,11 @@ public interface DishMapper {
      * @return
      */
     List<Dish> queryByCategory(Dish dish);
+
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
